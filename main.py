3aeb7c312c141bd004747ae495290747494a48cc
kallolPratim <kallolpratimsaikia@Macs-MacBook-Pro.local>
2023-04-24 23:32:14 +0530

    api baseurl added

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 from ai import generate_text
 
 
 app = FastAPI()
+router = APIRouter(prefix="/api/v1")
 
 
-@app.get("/")
+@router.get("/hello")
 async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/calculate")
+@router.get("/calculate")
 async def calculate():
     text = generate_text("I love you")
     return {"message": text}
+
+app.include_router(router)
